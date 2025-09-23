f5ad259f458861628062a3748de3d38eed431ea2
Jinyoung <glitt5384@naver.com>
2025-09-23 12:44:27 +0900

    feat: 필요 의존성 설치

---
+++ src/main/java/com/portmate/PortMateApplication.java
@@ -0,0 +1,13 @@
+package com.portmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portmate/PortMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
