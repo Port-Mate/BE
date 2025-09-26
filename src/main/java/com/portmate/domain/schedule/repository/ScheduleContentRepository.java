acb8a6e349a860587e987ec044bc47fc8deda84a
Jinyoung <glitt5384@naver.com>
2025-09-26 14:25:47 +0900

    delete

--- src/main/java/com/portmate/domain/schedule/repository/ScheduleContentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.portmate.domain.schedule.repository;
-
-import com.portmate.domain.schedule.entity.ScheduleContent;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScheduleContentRepository extends MongoRepository<ScheduleContent, String> {
-}
