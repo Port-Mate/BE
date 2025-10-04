0675f775f46ee6f9efc71954f07f428e873a2bab
Jinyoung <glitt5384@naver.com>
2025-10-04 22:42:27 +0900

    feat: 상태 만족하는 유저 목록 반환 및 상태 변경 API

--- src/main/java/com/portmate/domain/user/controller/UserController.java
+++ src/main/java/com/portmate/domain/user/controller/UserController.java
@@ -2,19 +2,29 @@ package com.portmate.domain.user.controller;
 
 import com.portmate.domain.user.dto.RegisterRequest;
 import com.portmate.domain.user.dto.UserInfoResponse;
+import com.portmate.domain.user.dto.UserStatusRequest;
+import com.portmate.domain.user.dto.UserStatusResponse;
+import com.portmate.domain.user.entity.User;
 import com.portmate.domain.user.service.UserService;
 import com.portmate.global.auth.CustomUserDetails;
 import com.portmate.global.response.ApiResponse;
+import com.portmate.global.response.PageResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Validated
 public class UserController {
     private final UserService userService;
 
@@ -23,9 +33,27 @@ public class UserController {
         return ResponseEntity.ok(ApiResponse.onSuccess(userService.fetchUserInfo(customUserDetails)));
     }
 
+    @GetMapping("/pending")
+    public ResponseEntity<ApiResponse<PageResponse<List<UserStatusResponse>>>> getPendingUsers(
+            @RequestParam(defaultValue = "1") @Min(value = 1) int page,
+            @RequestParam(defaultValue = "10") @Min(value = 1) int size
+    ){
+        Pageable pageable = PageRequest.of(page - 1, size);
+        return ResponseEntity.ok(ApiResponse.onSuccess(userService.queryByStatus(User.UserStatus.PENDING, pageable)));
+    }
+
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<Void>> postRegister(@Validated @RequestBody RegisterRequest registerRequest){
         userService.insertNewUser(registerRequest);
         return ResponseEntity.ok(ApiResponse.onSuccessVoid());
     }
+
+    @PutMapping("/{userId}/status")
+    public ResponseEntity<ApiResponse<Void>> putUserStatus(@PathVariable("userId") String userId,
+                                                           @RequestBody UserStatusRequest request){
+        userService.updateUserStatus(userId, request);
+        return ResponseEntity.ok(ApiResponse.onSuccessVoid());
+    }
+
+
 }
