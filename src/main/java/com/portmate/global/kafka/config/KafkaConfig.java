6d93a557a26c245c8d5022335941e3a52e871c32
7ijin01 <dugudada01@naver.com>
2025-10-11 00:38:33 +0900

    test

--- src/main/java/com/portmate/global/kafka/config/KafkaConfig.java
+++ src/main/java/com/portmate/global/kafka/config/KafkaConfig.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
 
-	@Value("${spring.kafka.bootstrap-servers}")
+	@Value("${spring.kafka.bootstrap-servers:kafka:9092}")
 	private String bootstrapServers;
 
 	@Bean
