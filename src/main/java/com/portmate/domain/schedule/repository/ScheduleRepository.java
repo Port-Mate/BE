1d273a1705009a3688d6f4c11cf03db79dc04baa
Jinyoung <glitt5384@naver.com>
2025-10-11 16:19:19 +0900

    feat: 커스텀 레포 상속

--- src/main/java/com/portmate/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/portmate/domain/schedule/repository/ScheduleRepository.java
@@ -1,9 +1,10 @@
 package com.portmate.domain.schedule.repository;
 
 import com.portmate.domain.schedule.entity.Schedule;
+import com.portmate.domain.schedule.repository.custom.CustomScheduleRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ScheduleRepository extends MongoRepository<Schedule, String> {
+public interface ScheduleRepository extends MongoRepository<Schedule, String>, CustomScheduleRepository {
 }
