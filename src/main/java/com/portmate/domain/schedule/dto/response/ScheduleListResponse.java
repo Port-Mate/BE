3632c407e3602f03aa040a3bf4da304ccd3b88a6
7ijin01 <dugudada01@naver.com>
2025-10-13 22:20:18 +0900

    fix: port로 필드 수정

--- src/main/java/com/portmate/domain/schedule/dto/response/ScheduleListResponse.java
+++ src/main/java/com/portmate/domain/schedule/dto/response/ScheduleListResponse.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public record ScheduleListResponse(
         String scheduleId,
+        String portName,
         String pierName,
         String berthName,
         LocalDate startDt,
@@ -15,10 +16,21 @@ public record ScheduleListResponse(
         List<TimeTableWrapper> timeTableWrappers
 ) {
     public static ScheduleListResponse from(Schedule schedule, List<TimeTableWrapper> items) {
+        String pier = null;
+        String berth = null;
+        if (schedule.getScheduleContents() != null && !schedule.getScheduleContents().isEmpty()) {
+            pier = schedule.getScheduleContents().get(0).getPier();
+            berth = schedule.getScheduleContents().get(0).getBerth();
+        }
+
         return new ScheduleListResponse(
-                schedule.getScheduleId(), schedule.getPier(),
-                schedule.getBerth(), schedule.getStartDt(),
-                schedule.getEndDt(), items
+            schedule.getScheduleId(),
+            schedule.getPort(),
+            pier,
+            berth,
+            schedule.getStartDt(),
+            schedule.getEndDt(),
+            items
         );
     }
 }
